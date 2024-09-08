d9a92dc3e2b0304cc752d5372c06a7ce73b50a08
Bichão <brunobichao@gmail.com>
2024-09-08 20:06:12 -0300

    Add files via upload

---
+++ ConversorHash2.py
@@ -0,0 +1,29 @@
+import hashlib
+
+def gerar_hash(arquivotxt, hash_algo='md5'):
+    hash_obj = hashlib.new(hash_algo)
+    with open(arquivotxt, 'rb') as file:
+        while chunk := file.read(8192):
+            hash_obj.update(chunk)
+    return hash_obj.hexdigest()
+
+algoritmos_disponiveis = hashlib.algorithms_available
+print(f"Algoritmos de hash disponíveis: {', '.join(algoritmos_disponiveis)}")
+
+arquivotxt = input(r'Digite o caminho do arquivo:')
+hash_algo = input(f"Digite o algoritmo de hash desejado (padrão é 'md5'): ") or 'md5'
+
+if hash_algo not in algoritmos_disponiveis:
+    print(f"Algoritmo {hash_algo} não é suportado. Usando 'md5' como padrão.")
+    hash_algo = 'md5'
+
+print(f"""
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+                                Esse é o arquivo que você quer criptografar, correto?              
+                                  ({arquivotxt})                                     
+
+                                         Aqui está o Hash {hash_algo} dele:                      
+{gerar_hash(arquivotxt, hash_algo)}                           
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+""")
+input('Pressione <enter> para encerrar!')
